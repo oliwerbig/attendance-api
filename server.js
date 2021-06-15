b6f9142f3e7f9ea69a8db798e779da2b83aa611b
Oliver Emanuel Nagy <oliver.emanuel.nagy@gmail.com>
2021-06-15 13:44:03 +0200

    Replaced bodyParser

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require("express")
+const cors = require("cors")
 
-require('dotenv').config();
+require('dotenv').config()
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded())
 
-const db = require('./app/db');
+const db = require('./app/db')
 db
   .sequelize
   .sync(/*{ force: true }*/)
   .then(() => console.log('done'))
 
-require("./app/routes/group.routes")(app);
-require("./app/routes/session.routes")(app);
-require("./app/routes/student.routes")(app);
-require("./app/routes/attendance.routes")(app);
+require("./app/routes/group.routes")(app)
+require("./app/routes/session.routes")(app)
+require("./app/routes/student.routes")(app)
+require("./app/routes/attendance.routes")(app)
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`)
 });
